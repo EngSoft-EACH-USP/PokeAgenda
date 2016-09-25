2480ccdc1063d4354df5c6db74ea4115e630c304
alexsanderyokoyama <alexsanderyokoyama@gmail.com>
2016-09-25 00:13:32 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'application#helloworld"
+  root 'application#helloworld'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
