243ec4363f20740d1b5b0ab5989be53dfbffaab1
Henrique Okomura <henrique.okomura@dev.codus.com.br>
2016-10-04 20:33:38 -0300

    Acceptance tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def helloworld
-    render html: "Hello, world!"
   end
 
   protect_from_forgery with: :exception
---
+++ spec/acceptance/users/authentication_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+context "As a user", type: :request do
+  specify "I want to register my self" do
+    visit new_user_registration_path
+
+    fill_in "user_email", with: "email@test.com"
+    fill_in "user_password", with: "123123"
+    fill_in "user_password_confirmation", with: "123123"
+
+    click_button "Sign up"
+
+    expect(page).to have_content "logout"
+  end
+
+  specify "I want to login my self" do
+    user = FactoryGirl.create(:user, password: "123123", password_confirmation: "123123")
+
+    visit new_user_session_path
+
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: "123123"
+
+    click_button "Log in"
+
+    expect(page).to have_content "logout"
+  end
+end
