bfa07e806fbb289a6e848d3c98f0ea9cb4769f0b
lordlittlepants <matheus.pinheiro@usp.br>
2016-11-19 00:00:49 +0000

    Fixed hard error on production (bad pokemon.rb file).

--- spec/factories/pokemons.rb
+++ spec/factories/pokemons.rb
@@ -1,11 +1,6 @@
 FactoryGirl.define do
   factory :pokemon do
-<<<<<<< HEAD
     name "MyString"
     number 1
-=======
-    index 1
-      "MyString"
->>>>>>> 370f34301690daad393e03d97a2f2bd81c45ebba
   end
 end
